b5a639161f598bc64bac1d4badd248d4bb18875b
Aimee McVey <aimee.mcvey@duke.edu>
2020-02-05 18:32:28 -0500

    Add basic functionality to function

--- tachycardia.py
+++ tachycardia.py
@@ -3,6 +3,10 @@ def is_tachycardic():
     word = input("Input word: ")
     # could be upper, lower, mixed case
     # or have leading/trailing spaces or punctuation
+    print("{}" .format(word))
+    if word == "tachycardic":
+        print("True")
+        return True
 
 
 if __name__ == "__main__":
