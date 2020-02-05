14621fe15b426a2a718f8d4701fcb93f717e15f2
Aimee McVey <aimee.mcvey@duke.edu>
2020-02-05 18:22:02 -0500

    Create is_tachycardic function

--- tachycardia.py
+++ tachycardia.py
@@ -1,1 +1,9 @@
 # tachycardia.py
+def is_tachycardic():
+    word = input("Input word: ")
+    # could be upper, lower, mixed case
+    # or have leading/trailing spaces or punctuation
+
+
+if __name__ == "__main__":
+    is_tachycardic()
