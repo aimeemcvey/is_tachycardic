c7e10bb48534540d0a88359531bfa98130f24bc5
Aimee McVey <aimee.mcvey@duke.edu>
2020-02-05 19:21:10 -0500

    Add test case for exact input

--- test_tachycardia.py
+++ test_tachycardia.py
@@ -1,1 +1,8 @@
 # test_tachycardia.py
+
+
+def test_is_tachycardic_normal():
+    from tachycardia import is_tachycardic
+    answer = is_tachycardic("tachycardic")
+    expected = True
+    assert answer == expected
