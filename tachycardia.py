68fcdb3233575af8a3e1e9263c8e1865757b628b
Aimee McVey <aimee.mcvey@duke.edu>
2020-02-06 13:53:00 -0500

    Added loop to match letters - not working

--- tachycardia.py
+++ tachycardia.py
@@ -2,7 +2,6 @@
 
 
 def is_tachycardic(word):
-    print("{}".format(word))
     # could be upper, lower, mixed case
     word = word.lower()
     # or have punctuation
@@ -11,14 +10,14 @@ def is_tachycardic(word):
     word = word.translate(table)
     # or have leading/trailing spaces
     word = word.strip()
-    print("{}".format(word))
     if word == "tachycardic":
         print("True")
         return True
     else:
         spellcheck(word)
-        print("False")
-        return False
+        #print("False")
+        #return False
+
 
 def spellcheck(word):
     target_word = []
@@ -27,9 +26,21 @@ def spellcheck(word):
         target_word.append(char)
     for char in word:
         my_word.append(char)
-    # if 9/11 chars match, return True
     print(target_word)
     print(my_word)
+    # if 9/11 chars match, return True
+    x, y = 0, 0
+    for i in word:
+        if "tachycardic".find(i) >= 0 and y == word.find(i):
+            x += 1
+            print(i, x)
+        y += 1
+    if x >= 9:
+        print("True")
+        return True
+    else:
+        print("False")
+        return False
 
 
 if __name__ == "__main__":
