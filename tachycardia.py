fc10cc0fee634497fb07bc6277aad234c2c7a6a1
Aimee McVey <aimee.mcvey@duke.edu>
2020-02-07 22:16:24 -0500

    Allow words w 2 letters wrong to return True

--- tachycardia.py
+++ tachycardia.py
@@ -15,26 +15,15 @@ def is_tachycardic(word):
         return True
     else:
         spellcheck(word)
-        #print("False")
-        #return False
 
 
 def spellcheck(word):
-    target_word = []
-    my_word = []
-    for char in "tachycardic":
-        target_word.append(char)
-    for char in word:
-        my_word.append(char)
-    print(target_word)
-    print(my_word)
     # if 9/11 chars match, return True
-    x, y = 0, 0
+    x = 0
     for i in word:
-        if "tachycardic".find(i) >= 0 and y == word.find(i):
+        if i in "tachycardic":
             x += 1
             print(i, x)
-        y += 1
     if x >= 9:
         print("True")
         return True
